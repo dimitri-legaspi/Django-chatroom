58a06cc8a276781bfcd8165e7393ffe5fa070c82
Dimitri <dimitri.legaspi@gmail.com>
2024-01-16 16:25:18 -0500

    Created the get function to acess all the rooms

--- Chatroom/rooms/views.py
+++ Chatroom/rooms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . models import Room
 
 # Create your views here.
+
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
